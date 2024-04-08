e711ac898e2d5f40f084df8ae1cdbdb152748679
Nguyễn Phước Ngọc Hương <minanguyen09102004@gmail.com>
2024-04-08 19:53:22 +0700

    Tạo xong #23 không xóa branch!!

---
+++ Project/src/app/controllers/JourneyController.js
@@ -0,0 +1,31 @@
+const journey = require("../models/Journey");
+const driver = require("../models/Driver");
+const { json } = require("express");
+
+class JourneyController {
+  // [GET]: display all journeys' information
+  async index(req, res) {
+    try {
+      const journeys = await journey.find({});
+      res.json(journeys);
+      console.log(journeys);
+    } catch (error) {
+      res.status(500).json({ err: "ERROR" });
+    }
+  }
+
+  addDriver(req, res) {
+    //console.log(req.params.slug);
+    journey.findOne({ Transportation: req.params.slug }).then((j) => {
+      console.log(j);
+      driver.findOne({ Name: "Tran fish" }).then((driver1) => {
+        j.Driver.push(driver1);
+        j.save();
+        console.log(j);
+        res.send("yeah!!!");
+      });
+    });
+  }
+}
+
+module.exports = new JourneyController();
--- Project/src/routes/index.js
+++ Project/src/routes/index.js
@@ -1,8 +1,11 @@
 const adminRouter = require("./admin");
 const homeRouter = require("./home");
+const journeyRouter = require("./journey");
 
 function route(app) {
   app.use("/admin", adminRouter);
+
+  app.use("/journey", journeyRouter);
 
   app.use("/", homeRouter);
 }
