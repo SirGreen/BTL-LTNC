2d80f3255192d7b9722630662ae59b993f33fef2
Nguyễn Phước Ngọc Hương <minanguyen09102004@gmail.com>
2024-04-20 13:20:26 +0700

    Add-update-API-for-Admin-33-6

--- Project/src/routes/admin.js
+++ Project/src/routes/admin.js
@@ -6,7 +6,11 @@ const adminController = require("../app/controllers/AdminController");
 router.use("/addTransportation", adminController.AddNewTransportation);
 router.use("/addDriver", adminController.AddNewDriver);
 router.use("/addJourney", adminController.AddJourney);
-router.use("/addAdmin",adminController.AddAdmin);
+router.use("/addAdmin", adminController.AddAdmin);
+router.use("/update/driver/:id", adminController.UpdateDriver);
+router.use("/update/car/:id", adminController.UpdateCar);
+router.use("/update/truck/:id", adminController.UpdateTruck);
+router.use("/update/coach/:id", adminController.UpdateCoach);
 router.use("/", adminController.index);
 
 module.exports = router;
