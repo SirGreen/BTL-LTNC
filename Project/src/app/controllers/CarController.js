9e3be2fe2ef0a3c2555fca263605df7c136837f5
Nguyễn Phước Ngọc Hương <minanguyen09102004@gmail.com>
2024-04-23 20:22:04 +0700

    finish 33-8

--- Project/src/app/controllers/CarController.js
+++ Project/src/app/controllers/CarController.js
@@ -1,23 +1,42 @@
-const car = require('../models/Car')
+const car = require("../models/Car");
+const ObjectId = require("mongodb").ObjectId;
 
 class CarController {
-    //[GET] /
-    index(req, res) {
-        res.send("Car page")
-    }
+  //[GET] /
+  index(req, res) {
+    res.send("Car page");
+  }
 
-    //[GET] /infomation
-    async DisplayInfo(req, res)
-    {
-      try {
-        const cars = await car.find({});
-        res.json(cars);
-        console.log(cars);
-      } catch (error) {
-        res.status(500).json({ err: "ERROR" });
-      }
+  //[GET] /infomation
+  async DisplayInfo(req, res) {
+    try {
+      const cars = await car.find({});
+      res.json(cars);
+      console.log(cars);
+    } catch (error) {
+      res.status(500).json({ err: "ERROR" });
     }
+  }
+
+  //[GET] /information about a specific car
+  async ShowThis(req, res) {
+    try {
+      if (!ObjectId.isValid(req.params.id)) {
+        res.send("Invalid car ID");
+        return;
+      }
+      var thisCar = null;
+      thisCar = await car.findOne({ _id: req.params.id });
+      if (thisCar == null) {
+        res.send("No car with that ID!!!");
+        return;
+      }
+      res.json(thisCar);
+      console.log(thisCar);
+    } catch (error) {
+      res.status(500).json({ err: "ERROR: Invalid ID" });
+    }
+  }
 }
-  
+
 module.exports = new CarController();
-  
--- Project/src/app/controllers/CoachController.js
+++ Project/src/app/controllers/CoachController.js
@@ -1,23 +1,41 @@
-const coach = require('../models/Car')
+const coach = require("../models/Coach");
+const ObjectId = require("mongodb").ObjectId;
 
 class CoachController {
-    //[GET] /
-    index(req, res) {
-        res.send("Coach page")
-    }
+  //[GET] /
+  index(req, res) {
+    res.send("Coach page");
+  }
 
-    //[GET] /infomation
-    async DisplayInfo(req, res)
-    {
-      try {
-        const coaches = await car.find({});
-        res.json(coaches);
-        console.log(coaches);
-      } catch (error) {
-        res.status(500).json({ err: "ERROR" });
-      }
+  //[GET] /infomation
+  async DisplayInfo(req, res) {
+    try {
+      const coaches = await coach.find({});
+      res.json(coaches);
+      console.log(coaches);
+    } catch (error) {
+      res.status(500).json({ err: "ERROR" });
     }
+  }
+  //[GET] /information about a specific coach
+  async ShowThis(req, res) {
+    try {
+      if (!ObjectId.isValid(req.params.id)) {
+        res.send("Invalid coach ID");
+        return;
+      }
+      var thiscoach = null;
+      thiscoach = await coach.findOne({ _id: req.params.id });
+      if (thiscoach == null) {
+        res.send("No coach with that ID!!!");
+        return;
+      }
+      res.json(thiscoach);
+      console.log(thiscoach);
+    } catch (error) {
+      res.status(500).json({ err: "ERROR: Invalid ID" });
+    }
+  }
 }
-  
+
 module.exports = new CoachController();
-  
