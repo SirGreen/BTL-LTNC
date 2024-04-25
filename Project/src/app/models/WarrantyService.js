0278e7e884fbf1b90d4601135bce710fb16fbd4a
Le Tran <lethanhbaotran.letran@gmail.com>
2024-04-25 17:35:43 +0700

    Complete check income & warranty Service #33-7

--- Project/src/app/models/WarrantyService.js
+++ Project/src/app/models/WarrantyService.js
@@ -3,14 +3,7 @@ const Schema = mongoose.Schema
 
 function threeMonthFromNow() {
     let d = new Date();
- 
-    console.log("Today's Date: "
-        + d.toLocaleDateString());
-    
     d.setMonth(d.getMonth() + 3);
- 
-    console.log("3 months Prior Date: "
-        + d.toLocaleDateString());
     return d;
 }
 
