46f8a8f04c61cf2cb376a243629825bc003945a4
SirGreen <61875709+SirGreen@users.noreply.github.com>
2024-04-29 19:43:33 +0700

    fixed clearAll button

--- Project/src/resources/js/map.js
+++ Project/src/resources/js/map.js
@@ -182,10 +182,14 @@ require([
 
 //outside
 function clearAll(event) {
-  event.preventDefault();
-  console.log("Clearing map");
-  routeParams.stops.features = [];
-  routeLayer.removeAll();
+  // event.preventDefault();
+  // console.log("Clearing map");
+  // routeParams.stops.features = [];
+  // routeLayer.removeAll();
+  // var iframe = document.getElementById("myFrame");
+  // Reload the iframe content
+  // iframe.contentWindow.location.reload();
+  window.parent.location.reload()
 }
 
 async function send(event) {
